8fa11eb3626e3f1ecd478c1ed8685d2ffcc29fe4
Taylan Sen <tsen@cs.rochester.edu>
2018-10-19 07:13:49 -0400

    added all complete msg

--- cf.py
+++ cf.py
@@ -1,21 +1,29 @@
+#!/usr/bin/env python3
+"""
+-------------------------------------------------------------------------------
+Simple example on how python's concurrent.futures can be used to parallelize,
+for example, processing multiple files with os.system calls.
+-------------------------------------------------------------------------------
+"""
+
 import concurrent.futures as cf
 import glob
 import os
 
 
-NUM_CPU = 2
+NUM_CPU = 2 # use None to use all CPUs 
+
 def worker(f):
     print('im am the worker:', f)
     os.system('echo os call')
     os.system('sleep 5')
-    return 'worker: ' + f + ' complete'
+    return 'worker ' + f + ' completed'
 
-def main():
-    files = glob.glob('test/*')
-
-    with cf.ProcessPoolExecutor(max_workers=NUM_CPU) as ex:       
-        for f,result in zip(files, ex.map(worker,files)):
-            print('result returned from %s: %s' %(f, result))
 
 if __name__ == '__main__':
-    main()
+    args = glob.glob('test/*')
+    with cf.ProcessPoolExecutor(max_workers=NUM_CPU) as ex:       
+        for arg,result in zip(args, ex.map(worker,args)):
+            print('worker returned (arg:%s, result: %s)' %(arg, result))
+    print('all processes completed')
+
